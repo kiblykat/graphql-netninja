808698cce052cc3bccdc5dc95229cdaddb6f738c
izzat <izzatfadzlon@gmail.com>
2025-06-15 13:16:11 +0800

    lesson 5: resolver functions

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,33 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema.js";
+import db from "./_db.js";
+
+// define resolver functions defined in the root Query type
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    reviews() {
+      return db.reviews;
+    },
+    authors() {
+      return db.authors;
+    },
+  },
+};
 
 // server setup using ApolloServer - takes an object with 2 properties
 const server = new ApolloServer({
   //typeDefs: descriptions of datatypes and relationships with other datatypes (make own Schema)
+  typeDefs,
   //resolvers: define how to respond to queries for different data on the graph
+  resolvers,
 });
 
 // server startup using startStandaloneServer
-const { url } = startStandaloneServer(server, {
+const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
 });
 
---
+++ src/schema.ts
@@ -0,0 +1,27 @@
+export const typeDefs = `#graphql
+ type Game {
+  id: ID!
+  title: String!
+  platform: [String!]!
+ }
+
+ type Review {
+  id: ID!
+  rating: Int!
+  content: String!
+ }
+ 
+ type Author {
+  id: ID!
+  name: String!
+  verified: Boolean!
+ }
+ type Query {
+  reviews: [Review]
+  games: [Game]
+  authors: [Author]
+ }
+`;
+
+// 5 types: Int, Float, String, Boolean, ID
+// Query: gatekeeps the entrypoint into the graph, prevent user from entering from anywhere
